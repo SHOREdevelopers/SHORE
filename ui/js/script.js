847ac1ca8078701d00d7fe53f7a4f433d8567346
Mark Reeves <mjr@clearbold.com>
2016-09-01 11:16:08 -0400

    Nav fix

--- ui/js/script.js
+++ ui/js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     $('.nav-bar > div > div').on('mouseenter', function(e) {
         e.preventDefault();
 
-        $(this).children('div').animate( { height: ["toggle", "swing"] } );
+        $(this).children('div').stop().animate( { height: ["toggle", "swing"] } );
     })
 
     $('.nav-bar > div > div').on('mouseleave', function(e) {
         e.preventDefault();
 
-        $(this).children('div').animate( { height: ["toggle", "swing"] } );
+        $(this).children('div').stop().animate( { height: ["toggle", "swing"] } );
     })
 
     $('div.menu-nav-button').on('click', function(e) {
